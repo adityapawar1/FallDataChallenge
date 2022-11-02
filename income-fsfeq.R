fd313753e36838c6f57d976f1787e8aefa17aebd
Akhil Datla <66145155+akhil-datla@users.noreply.github.com>
2022-11-02 10:16:02 -0700

    Removed outliers and reordered x-axis for income-fsfreq.R

--- income-fsfeq.R
+++ income-fsfeq.R
@@ -6,10 +6,13 @@ library(ggplot2)
 library(dplyr)
 library(forcats)
 
-data <- read.csv("/Users/akhildatla/Documents/ADA/data.csv")
+fall_data <- read.csv("/Users/akhildatla/Documents/ADA/data.csv")
 
-data %>%
-  mutate(name = fct_reorder(as.character(TTLHHINC), FSFREQ, .fun = 'median')) %>%
+x<-quantile(fall_data$FSFREQ,c(0.10,0.90))
+data_clean <- fall_data[fall_data$FSFREQ >=x[1] & fall_data$FSFREQ<=x[2],]
+
+data_clean %>%
+  mutate(name = factor(TTLHHINC)) %>%
   ggplot( aes(x=name, y=FSFREQ, fill=as.character(TTLHHINC))) + 
   geom_boxplot() +
   xlab("class") +
