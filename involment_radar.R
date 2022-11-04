d66e596e43ecfb87735184ff10dc7e8e22987f69
Aditya Pawar <aditya.pawar@warriorlife.net>
2022-11-03 20:39:35 -0700

    Fix radar

--- involment_radar.R
+++ involment_radar.R
@@ -48,49 +48,8 @@ means <- c(
 bar_df <- data.frame(target_columns, means)
 print(bar_df)
 
-plt <- ggplot(plot_df) +
-  # Make custom panel grid
-  geom_hline(
-    aes(yintercept = y), 
-    data.frame(y = c(0:3) * 1000),
-    color = "lightgrey"
-  ) + 
-  # Add bars to represent the cumulative track lengths
-  # str_wrap(region, 5) wraps the text so each line has at most 5 characters
-  # (but it doesn't break long words!)
-  geom_col(
-    aes(
-      x = reorder(str_wrap(target_columns, 7), 0),
-      y = 0,
-      fill = n
-    ),
-    position = "dodge2",
-    show.legend = TRUE,
-    alpha = .9
-  ) +
-  
-  # Add dots to represent the mean gain
-  geom_point(
-    aes(
-      x = reorder(str_wrap(target_columns, 7), 0),
-      y = 0
-    ),
-    size = 3,
-    color = "gray12"
-  ) +
-  
-  # Lollipop shaft for mean gain per region
-  geom_segment(
-    aes(
-      x = reorder(str_wrap(target_columns, 7), 0),
-      y = 0,
-      xend = reorder(str_wrap(target_columns, 7), 0),
-      yend = 3000
-    ),
-    linetype = "dashed",
-    color = "gray12"
-  ) + 
-
+plt <- ggplot(bar_df, aes(x=target_columns, y=means)) + 
+  geom_bar(stat = "identity") +
   coord_polar()
 
 plt
